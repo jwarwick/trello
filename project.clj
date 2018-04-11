afeefd70e510fc155a3fb4d7c1bc330629dcee3a
John Warwick <jwarwick@gmail.com>
2018-04-11 13:11:54 +0200

    Update project file

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject me.bsima/trello "0.3.0"
-  :description "Clojure wrapper for the Trello API"
-  :url "https://github.com/bsima/trello"
+(defproject jwarwick/trello "0.3.1"
+  :description "Clojure wrapper for the Trello API (forked from me.bsima/trello)"
+  :url "https://github.com/jwarwick/trello"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "2.0.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-http "3.7.0"]
                  [clj-oauth "1.5.2"]
                  [org.clojure/core.typed "0.3.11"]
                  [org.clojure/data.json "0.2.5"]
                  [org.clojure/tools.cli "0.3.3"]]
   :scm {:name "git"
-        :url "https://github.com/bsima/trello"})
+        :url "https://github.com/jwarwick/trello"})
